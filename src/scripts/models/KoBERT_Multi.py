81302a6fdf423c5f58e39484d188198f83002ce9
wonnerky <gandet09@gmail.com>
2021-10-04 07:59:07 +0000

    default setting

---
+++ src/scripts/models/KoBERT_Multi.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+from lib.kobert.pytorch_kobert import get_pytorch_kobert_model
+
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 hidden_size=768,
+                 num_classes=7,
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        bert, _ = get_pytorch_kobert_model()
+        self.bert = bert
+        self.dr_rate = dr_rate
+        self.classifier = nn.Linear(hidden_size, num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+
+    def gen_attention_mask(self, token_ids):
+        attn_mask = ~torch.eq(token_ids, 1)
+        return attn_mask.float()
+
+    def forward(self, token_ids):
+        attention_mask = self.gen_attention_mask(token_ids)
+
+        _, pooler = self.bert(input_ids=token_ids, attention_mask=attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
---
+++ src/scripts/requirements.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install --yes python3-pip
+sudo pip install -r src/scripts/requirements.txt
