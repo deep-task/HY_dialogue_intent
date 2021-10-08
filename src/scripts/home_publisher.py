273eec0ab9baf75c0fca86ba09a826cf1e198260
wonnerky <gandet09@gmail.com>
2021-10-08 14:30:41 +0900

    ros msg fix

--- src/scripts/home_publisher.py
+++ src/scripts/home_publisher.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 
 
 def talker():
-    pub = rospy.Publisher('/intentExecution', String, queue_size=10)
+    pub = rospy.Publisher('/taskExecution', String, queue_size=10)
 
     rospy.init_node('dm_intent_node', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
@@ -41,8 +41,8 @@ def talker():
     # publish_str_ko_home.append(
     #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "saying_good_bye", "id": 208, "human_speech": "알겠어. 고마워.", "social_context": {}}}')
 
-    publish_str_ko_home.append(
-        '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"],"source": "planning"},"dialog_intent": {"name": "이병현", "intent": "saying_hello", "id": 196, "human_speech": "안녕","social_context": {"appellation":"할아버지"}}}')
+    # publish_str_ko_home.append(
+    #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"],"source": "planning"},"dialog_intent": {"name": "이병현", "intent": "saying_hello", "id": 196, "human_speech": "안녕","social_context": {"appellation":"할아버지"}}}')
     # publish_str_ko_home.append(
     #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "check_information_sleep", "id": 197, "human_speech": "응 그래.","social_context": {}}}')
     # publish_str_ko_home.append(
@@ -52,19 +52,22 @@ def talker():
     # publish_str_ko_home.append(
     #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "check_information_meal", "id": 200, "human_speech": "안 좋아졌어. 나빠.", "social_context": {}}}')
     # publish_str_ko_home.append(
-    #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "transmit_information_disease_advice", "id": 201, "human_speech": "짜게 먹었어", "social_context": {"disease_name":"당뇨", "disease_advice":"가벼운 음식과 싱거운 운동"}}}')
+    #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "transmit_information_disease_advice", "id": 201, "human_speech": "싱겁게 먹었어", "social_context": {"disease_name":"당뇨", "disease_advice":"가벼운 운동과 싱거운 음식"}}}')
     # publish_str_ko_home.append(
     #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "check_information_health", "id": 202, "human_speech": "아니 몰라.", "social_context": {"task":"주사 복용"}}}')
     # publish_str_ko_home.append(
     #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "transmit_information_health_advice", "id": 203, "human_speech": "안 먹었어.", "social_context": {"medicine_schedule":"식후 3시간 후 복용"}}}')
     # publish_str_ko_home.append(
     #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "saying_good_bye", "id": 204, "human_speech": "알겠어. 고마워.", "social_context": {}}}')
-    # publish_str_ko_home.append(
-    #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "saying_hello", "id": 205, "human_speech": "또 보는구나", "social_context": {"appellation":"할아버지"}}}')
-    # publish_str_ko_home.append(
-    #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "check_information_schedule", "id": 205, "human_speech": "잘 못 지냈어", "social_context": {"visit_place":"약국"}}}')
-    # publish_str_ko_home.append(
-    #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "transmit_information_disease_regard", "id": 206, "human_speech": "안 갔어", "social_context": {"disease_name":"당뇨","disease_status":"negative", "appellation":"할아버지"}}}')
+    publish_str_ko_home.append(
+        '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"},    \
+            "dialog_intent": {"name": "이병현", "intent": "saying_hello", "id": 205, "human_speech": "또 보는구나", "social_context": {"appellation":"할아버지"}}}')
+    publish_str_ko_home.append(
+        '{"header": {"timestamp": "1563980674.262554407", "target": ["dialog"], "content": ["dialog_intent"], "source": "planning"},    \
+            "dialog_intent": {"name": "이병현", "intent": "check_information_schedule", "id": 205, "human_speech": "잘 지냈어", "social_context": {"visit_place":"약국"}}}')
+    publish_str_ko_home.append(
+        '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"},  \
+            "dialog_intent": {"name": "이병현", "intent": "transmit_information_disease_regard", "id": 206, "human_speech": "안 갔어", "social_context": {"disease_name":"당뇨","disease_status":"negative", "appellation":"할아버지"}}}')
     # publish_str_ko_home.append(
     #     '{"header": {"timestamp": "1563980674.262554407", "target": ["intent"], "content": ["dialog_intent"], "source": "planning"}, "dialog_intent": {"name": "이병현", "intent": "transmit_information_medicine", "id": 207, "human_speech": "그래?", "social_context": {"disease_name":"당뇨", "disease_status":"negative", "appellation":"할아버지"}}}')
     # publish_str_ko_home.append(
--- src/scripts/launch_dm_intent.py
+++ src/scripts/launch_dm_intent.py
@@ -1,74 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
 import rospkg
 import rospy, time
 from std_msgs.msg import String
 from intent_classifier import Model
 from google.protobuf import json_format
 import os, json
+from dtroslib.helpers import get_package_path, get_module_configuration, get_key_path
+from dtroslib.dialogflow import DialogflowClient
 
-test_path = rospkg.RosPack().get_path('dm_intent')
+
+test_path = get_package_path('dm_intent')
 # test_path = '..'
 
 # set homecare or reception
 version = 'homecare'
 
 model = Model(test_path + '/model/model_ckpt.ckpt')
-publisher = rospy.Publisher('/intentCompletion', String, queue_size=10)
+publisher = rospy.Publisher('/taskCompletion', String, queue_size=10)
 
-AUTH_KEY_HOMECARE_PATH = test_path + "/authkey/socialrobot-hyu-xdtlug-7fe2505e00b7.json"
-AUTH_KEY_RECEPTION_PATH = test_path + "/authkey/socialrobot-hyu-reception-nyla-a093501276ce.json"
 
-if version == "homecare":
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = AUTH_KEY_HOMECARE_PATH
-else:
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = AUTH_KEY_RECEPTION_PATH
+configuration = get_module_configuration('dm_intent', version)
 
-# [START dialogflow_detect_intent_text]
-def detect_intent_texts(project_id, session_id, texts, language_code):
-    """Returns the result of detect intent with texts as inputs.
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
+key_path = get_key_path('dm_intent', configuration['authorization']['key'])
 
-    from google.cloud import dialogflow_v2 as dialogflow
-    session_client = dialogflow.SessionsClient()
+dialogflow_client = DialogflowClient(project_id=configuration['project-id'],
+                                     session_id=configuration['session-id'],
+                                     key_path=key_path,
+                                     language_code=configuration['language-code'])
 
-    session = session_client.session_path(project_id, session_id)
-    print('Session path: {}\n'.format(session))
 
-    for text in texts:
-        text_input = dialogflow.types.TextInput(
-            text=text, language_code=language_code)
+def get_information(human_speech):
+    response = dialogflow_client.detect_intent_text(human_speech)
+    information = {}
 
-        query_input = dialogflow.types.QueryInput(text=text_input)
+    for key, val in response.query_result.parameters.items():
+        if type(val) != str:
+            val = list(val)
+        information[key] = val
 
-        response = session_client.detect_intent(
-            session=session, query_input=query_input)
-
-        print('=' * 20)
-        print('Query text: {}'.format(response.query_result.query_text))
-
-        entities = response.query_result.parameters
-        print(entities)
-        # entities_dic = json_format.MessageToDict(entities)
-        entities_dic = None
-        return entities_dic
+    return information
 
 
 def classify_intent(msg):
     ros_input = json.loads(msg.data, encoding='utf-8')
-    text = ros_input['dialog_intent']['human_speech']
-    out = model.inference(text) # res는 최종 label output
+    if "intent" in ros_input['header']['target']:
+        text = ros_input['dialog_intent']['human_speech']
+        out = model.inference(text) # res는 최종 label output
+        info = get_information(text)
 
-    if version == "homecare":
-        info = detect_intent_texts('socialrobot-hyu-xdtlug', 'socialrobot-hyu-xdtlug', [text], 'ko')  # homecare ko
-    else:
-        info = detect_intent_texts('socialrobot-hyu-reception-nyla', 'socialrobot-hyu-reception-nyla', [text], 'ko')  # reception ko
-    # info = None
-
-    out_msg = make_response_json(out, text, info)
-    print(out_msg)
-    publisher.publish(out_msg)
+        out_msg = make_response_json(out, text, info)
+        print(out_msg)
+    
+        publisher.publish(json.dumps(out_msg))
 
 
 def make_response_json(label, human_speech, information):
@@ -78,7 +63,7 @@ def make_response_json(label, human_speech, information):
             "content": [
                 "dialog_intent"
             ],
-            "source": "dialog",
+            "source": "intent",
             "target": [
                 "planning"
             ],
@@ -98,6 +83,6 @@ if __name__ == '__main__':
     rospy.init_node('dm_intent_node')
     rospy.loginfo('Start DM(intent)')
 
-    rospy.Subscriber('/intentExecution', String, classify_intent)
+    rospy.Subscriber('/taskExecution', String, classify_intent)
 
     rospy.spin()
