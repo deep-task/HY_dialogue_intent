7006e6c95a026cc6131e48670401d471a8fa94e3
wonnerky <gandet09@gmail.com>
2021-10-08 15:16:17 +0900

    edit entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,7 +24,7 @@ rm /tmp/cookie
 rm -rf authkey/
 rm -rf ckpt/
 unzip $FILENAME
-mv hyu_intent/authkey/configuration.json src/
+mv hyu_intent/authkey/configuration.json src/configuration.json
 mv hyu_intent/authkey src/
 mv hyu_intent/ckpt src/
 rm -r hyu_intent
