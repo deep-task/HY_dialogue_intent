8a95432a1c90f8751c8000097f695ff964a38319
wonnerky <gandet09@gmail.com>
2021-10-08 14:47:05 +0900

    add file download script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,24 @@ echo "source /workspace/devel/setup.sh" >> /etc/bash.bashrc
 # Setup executable files
 chmod +x src/scripts/launch_dm_intent.py
 
+# Download model files
+sudo apt-get install unzip
+sudo apt-get install curl
+echo "Model Download PATH : "pwd
+FILEID='1UnXusiOMAaoNnLy245QhQVSqlbRxetbB'
+FILENAME='hyu_intent.zip'
+curl -c /tmp/cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' /tmp/cookie`&id="${FILEID} -o ${FILENAME}
+rm /tmp/cookie
+rm -rf authkey/
+rm -rf ckpt/
+unzip $FILENAME
+mv hyu_intent/authkey .
+mv hyu_intent/ckpt .
+rm -r hyu_intent
+rm $FILENAME
+echo "Modelfile Download Complete!!"
+
 # Execute CMD
 exec "$@"
 
