b32920714277d5d939350c47ee697691222d6c1f
wonnerky <gandet09@gmail.com>
2021-10-08 02:49:37 +0000

    download file add

--- src/model_download.sh
+++ src/model_download.sh
@@ -2,23 +2,28 @@
 #!/bin/bash
 
 sudo apt-get install unzip
+sudo apt-get install curl
+
 
 echo "Model Download PATH : "pwd
-FILEID='1J9Nui4erfhVlCbcDejnu6LaoNRhCT9Tm'
-FILENAME='M2-7_dm_intent.zip'
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id="$FILEID > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id="$FILEID -o $FILENAME
-rm cookie
+FILEID='1UnXusiOMAaoNnLy245QhQVSqlbRxetbB'
+FILENAME='hyu_intent.zip'
+curl -c /tmp/cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' /tmp/cookie`&id="${FILEID} -o ${FILENAME}
+rm /tmp/cookie
 
-
-rm -rf scripts/authkey/
-rm -rf scripts/checkpoints/
-rm -rf scripts/data/
+rm -rf authkey/
+rm -rf ckpt/
 
 unzip $FILENAME
 
-mv DM_intent_hyu/* src/scripts/models/
+mv hyu_intent/authkey .
+mv hyu_intent/ckpt .
+
+rm -r hyu_intent
 
 rm $FILENAME
 
 echo "Modelfile Download Complete!!"
+
+
