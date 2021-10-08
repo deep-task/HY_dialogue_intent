793bdf7e0c075b968ebb71e1c932e885405068a8
wonnerky <gandet09@gmail.com>
2021-10-08 15:31:45 +0900

    edit entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,16 +16,16 @@ chmod +x src/scripts/launch_dm_intent.py
 sudo apt-get install unzip
 sudo apt-get install curl
 echo "Model Download PATH : "pwd
-FILEID='1UnXusiOMAaoNnLy245QhQVSqlbRxetbB'
+FILEID='1O_lDMEUMSZ7F0MbDHlw0T6FhQ6m66rm0'
 FILENAME='hyu_intent.zip'
 curl -c /tmp/cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' /tmp/cookie`&id="${FILEID} -o ${FILENAME}
 rm /tmp/cookie
-rm -rf authkey/
-rm -rf ckpt/
+rm -rf keys
+rm -rf ckpt
 unzip $FILENAME
-mv hyu_intent/authkey/configuration.json src/
-mv hyu_intent/authkey src/
+mv hyu_intent/keys/configuration.json src/
+mv hyu_intent/keys src/
 mv hyu_intent/ckpt src/
 rm -r hyu_intent
 rm $FILENAME
