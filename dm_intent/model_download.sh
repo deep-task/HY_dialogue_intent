0b670a49041bdedb404fd9e795049f124297ff57
wonnerky <gandet09@gmail.com>
2021-10-15 17:37:41 +0900

    Edit model download script

--- dm_intent/model_download.sh
+++ dm_intent/model_download.sh
@@ -1,29 +1,21 @@
    
 #!/bin/bash
-
 sudo apt-get install unzip
 sudo apt-get install curl
-
-
 echo "Model Download PATH : "pwd
-FILEID='1UnXusiOMAaoNnLy245QhQVSqlbRxetbB'
+FILEID='1O_lDMEUMSZ7F0MbDHlw0T6FhQ6m66rm0'
 FILENAME='hyu_intent.zip'
 curl -c /tmp/cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' /tmp/cookie`&id="${FILEID} -o ${FILENAME}
 rm /tmp/cookie
-
-rm -rf authkey/
-rm -rf ckpt/
-
+rm -rf keys
+rm -rf ckpt
 unzip $FILENAME
-
-mv hyu_intent/authkey .
-mv hyu_intent/ckpt .
-
+mv hyu_intent/keys/configuration.json dm_intent/
+mv hyu_intent/keys dm_intent/
+mv hyu_intent/ckpt dm_intent/
 rm -r hyu_intent
-
 rm $FILENAME
-
 echo "Modelfile Download Complete!!"
 
 
