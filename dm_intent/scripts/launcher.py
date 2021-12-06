91b5f79d73e63a99c9550e4332f538a8c9acd9c8
kist <bhu@kist.re.kr>
2021-12-06 16:52:58 +0900

    kist 환경 수정

--- dm_intent/scripts/launcher.py
+++ dm_intent/scripts/launcher.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 import json
 import rospkg
-import rospy, time
+import rospy
+import time
 from std_msgs.msg import String
 from intent_classifier import Model
 from google.protobuf import json_format
-import os, json
+import os
+import json
 from dtroslib.helpers import get_package_path, get_module_configuration, get_key_path
 from dtroslib.dialogflow import DialogflowClient
 from proto.marshal.collections.maps import MapComposite
@@ -18,7 +20,7 @@ test_path = get_package_path('dm_intent')
 version = 'homecare'
 
 model = Model(test_path + '/model/model_ckpt.ckpt')
-publisher = rospy.Publisher('/taskCompletion', String, queue_size=10)
+publisher = rospy.Publisher('/dialogResult', String, queue_size=10)
 
 
 configuration = get_module_configuration('dm_intent', version)
@@ -30,13 +32,14 @@ dialogflow_client = DialogflowClient(project_id=configuration['project-id'],
                                      key_path=key_path,
                                      language_code=configuration['language-code'])
 
+
 def get_information(human_speech):
     response = dialogflow_client.detect_intent_text(human_speech)
     information = {}
 
     for key, val in response.query_result.parameters.items():
         if isinstance(val, MapComposite):
-            val = dict(val) 
+            val = dict(val)
         elif type(val) != str:
             val = list(val)
         information[key] = val
@@ -46,24 +49,30 @@ def get_information(human_speech):
 
 def classify_intent(msg):
     ros_input = json.loads(msg.data, encoding='utf-8')
+    # if "dialog" in ros_input['header']['target']:
+    #     #df_intent = ros_input['dialog_generation']['intent']
+    #     print("1111111111")
+    #     df_intent = 'check_information_disease'
+    #     #if df_intent == 'check_information_disease' or df_intent == 'check_information_sleep_2' or df_intent == 'check_information_drink' or df_intent == 'check_information_smoke':
+    #     #    dialogflow_client.trigger_intent_event('check_information_health')
     if "dialog" in ros_input['header']['target']:
-        df_intent = ros_input['dialog_generation']['intent']
-        if df_intent == 'check_information_disease' or df_intent == 'check_information_sleep_2' or df_intent == 'check_information_drink' or df_intent == 'check_information_smoke':
-            dialogflow_client.trigger_intent_event('check_information_health')
-    if "dialog_intent" in ros_input['header']['target']:
+        print("22222222222222222222")
         text = ros_input['human_speech']['speech']
-        id = ros_input['header']['id']
-        out = model.inference(text) # res는 최종 label output
+        # id = ros_input['header']['id']
+        id = 31
+        print("/recognitionResult : 사람 발화 수신 완료")
+        out = model.inference(text)  # res는 최종 label output
         info = get_information(text)
 
         out_msg = make_response_json(out, text, id, info)
         print(out_msg)
-    
+
         publisher.publish(json.dumps(out_msg))
 
 
 def make_response_json(label, human_speech, id, information):
-    label_list = {0: '정보전달', 1: '인사', 2: '질문', 3: '요청', 4: '약속', 5: '수락', 6: '거부'}
+    label_list = {0: '정보전달', 1: '인사', 2: '질문',
+                  3: '요청', 4: '약속', 5: '수락', 6: '거부'}
     final_response = {
         "header": {
             "id": id,
@@ -89,6 +98,6 @@ if __name__ == '__main__':
     rospy.init_node('dm_intent_node')
     rospy.loginfo('Start DM(intent)')
 
-    rospy.Subscriber('/taskExecution', String, classify_intent)
+    rospy.Subscriber('/recognitionResult', String, classify_intent)
 
     rospy.spin()
